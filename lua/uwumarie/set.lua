bac7cdeb44d9c6c0345a104fc634708ab71c0558
Marie Ramlow <me@nycode.dev>
2023-01-14 17:29:10 +0100

    Use powershell as shell on windows

--- lua/uwumarie/set.lua
+++ lua/uwumarie/set.lua
@@ -13,5 +13,12 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 
-
-
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    vim.cmd [[
+        let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+		let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+		let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
+		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        set shellquote= shellxquote=
+        ]]
+end
