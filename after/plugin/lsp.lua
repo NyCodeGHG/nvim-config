438b165936455aee472380917ce77c71f8b08d17
Marie Ramlow <me@nycode.dev>
2023-01-16 00:17:06 +0100

    Add scroll docs mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,6 +16,8 @@ lsp.skip_server_setup({ 'rust_analyzer' })
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
