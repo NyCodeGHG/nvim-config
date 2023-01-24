2bc37e3598dde0c0b0a627a89f3890e3a0865d5e
Marie Ramlow <me@nycode.dev>
2023-01-24 11:51:25 +0100

    Start custom thingy

--- lua/uwumarie/plugins/alpha.lua
+++ lua/uwumarie/plugins/alpha.lua
@@ -2,8 +2,52 @@ return {
   {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = function ()
+    opts = function()
       return require('alpha.themes.dashboard').config
-    end
+    end,
+    -- opts = {
+    --   layout = {
+    --     { type = 'padding', val = 2 },
+    --     {
+    --       type = 'text',
+    --       val = {
+    --         [[               ,]],
+    --         [[             _/((]],
+    --         [[    _.---. .'   `\]],
+    --         [[  .'      `     ^ T=]],
+    --         [[ /     \       .--']],
+    --         [[|      /       )'-.]],
+    --         [[; ,   <__..-(   '-.)]],
+    --         [[ \ \-.__)    ``--._)]],
+    --         [[  '.'-.__.-.]],
+    --         [[    '-...-']],
+    --         [[]],
+    --         [[      Neovim ]],
+    --       },
+    --       opts = {
+    --         position = 'center',
+    --         hl = 'Type',
+    --       },
+    --     },
+    --     {
+    --       type = 'padding',
+    --       val = 2,
+    --     },
+    --     {
+    --       type = 'button',
+    --       val = 'New File',
+    --       on_press = function ()
+    --         print("new")
+    --       end,
+    --       opts = {
+    --         position = 'center',
+    --         shortcut = 'n',
+    --         cursor = 5,
+    --         width = 50,
+    --         align_shortcut = 'right',
+    --         hl_shortcut = 'Keyword',
+    --       }
+    --     },
+    --   },
   },
 }
