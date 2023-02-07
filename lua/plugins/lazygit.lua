feea339f2b47808d46f1a94e01bf582b768c15fd
Marie Ramlow <me@nycode.dev>
2023-02-07 16:39:49 +0100

    fix ssh agent again

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,16 +1,17 @@
-local Util = require("lazyvim.util")
-
 local command
-if vim.fn.has("win32") then
-  command = "lazygit"
+if vim.fn.has("unix") then
+  print("unix")
+  command = { "sh", "-icl", "lazygit" }
 else
-  command = { "sh", "-lic", "lazygit" }
+  command = "lazygit"
 end
 
 vim.keymap.set("n", "<leader>gg", function()
+  local Util = require("lazyvim.util")
   Util.float_term(command, { cwd = Util.get_root() })
 end, { desc = "Lazygit (root dir)" })
 
 vim.keymap.set("n", "<leader>gG", function()
+  local Util = require("lazyvim.util")
   Util.float_term(command)
 end, { desc = "Lazygit (cwd)" })
