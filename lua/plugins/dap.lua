aadccc3b717ebb252cb134472ffd8f0ccb58759f
Marie Ramlow <me@nycode.dev>
2023-02-23 05:03:17 +0100

    fix dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -112,8 +112,7 @@ return {
   {
     "mxsdev/nvim-dap-vscode-js",
     event = "BufReadPre *.js *.ts",
-    config = function()
-      require("dap-vscode-js").setup({
+    opts = {
         -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
         debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug/", -- Path to vscode-js-debug installation.
         -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
@@ -121,7 +120,8 @@ return {
         -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
         -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
         -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-      })
+    },
+    config = function()
       for _, language in ipairs({ "typescript", "javascript" }) do
         require("dap").configurations[language] = {
           {
